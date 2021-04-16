e53b1988f07c1075e366c8e66220bea1e27c9f27
Anton Tsybulka <anton.kurto@gmail.com>
2021-04-16 16:33:46 +0300

    add spinner

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import 'antd/dist/antd.css';
-import { Layout } from 'antd';
+import { Layout, Spin } from 'antd';
 
 import { getEmployeesThunk } from './redux/thunk/employeesThunk';
 import { HeaderConten, Home, Employees, } from './components/index';
@@ -10,7 +10,7 @@ import { HeaderConten, Home, Employees, } from './components/index';
 const { Content } = Layout;
 
 const App = () => {
-  const { employees } = useSelector((state) => state.employees),
+  const { employees, loading } = useSelector((state) => state.employees),
     dispatch = useDispatch();
 
   const data = employees &&
@@ -22,26 +22,34 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <Layout className='layout'>
-      <HeaderConten />
-      <Content className='site-layout' style={{ marginTop: 64 }}>
-        <div className='site-layout-background' style={{ padding: 10 }}>
-          <Switch>
-            <Route
-              path='/'
-              component={() => <Home />}
-              key='1'
-              exact />
-            <Route
-              path='/employees'
-              component={() => <Employees data={data} />}
-              key='2' />
-            <Redirect to='/' />
-          </Switch>
-        </div>
-      </Content>
-    </Layout>
-  );
+    <>
+      {loading ?
+        (<div style={{ width: '5%', margin: '15% auto' }}>
+          <Spin
+            tip='Loading...'
+            size='large' />
+        </div>) :
+        (<Layout className='layout'>
+          <HeaderConten />
+          <Content className='site-layout' style={{ marginTop: 64 }}>
+            <div className='site-layout-background' style={{ padding: 10 }}>
+              <Switch>
+                <Route
+                  path='/'
+                  component={() => <Home />}
+                  key='1'
+                  exact />
+                <Route
+                  path='/employees'
+                  component={() => <Employees data={data} />}
+                  key='2' />
+                <Redirect to='/' />
+              </Switch>
+            </div>
+          </Content>
+        </Layout>)}
+    </>
+  )
 };
 
 export default App;
